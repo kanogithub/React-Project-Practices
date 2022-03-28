d022f898d39c94e492c48abca758fef30f714c37
Wei <linkano.teck@gmail.com>
2022-03-28 17:15:53 +1100

    markdown notes finished

---
+++ notes-app/src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+function Sidebar({ notes, currentNote, setCurrentNoteId, newNote, deleteNote }) {
+	const noteElements = notes.map((note, index) => (
+		<div key={note.id}>
+			<div
+				className={`title ${note.id === currentNote.id ? 'selected-note' : ''}`}
+				onClick={() => setCurrentNoteId(note.id)}>
+				<h4 className='text-snippet'>{note.body.split('\n')[0]}</h4>
+				<button className='delete-btn' onClick={(event) => deleteNote(event, note.id)}>
+					<i className='gg-trash trash-icon'></i>
+				</button>
+			</div>
+		</div>
+	))
+
+	return (
+		<section className='pane sidebar'>
+			<div className='sidebar-header'>
+				<h3>MarkDown Notes</h3>
+				<button className='new-note' onClick={newNote}>
+					+
+				</button>
+			</div>
+			{noteElements}
+		</section>
+	)
+}
+
+export default Sidebar
