004d3d89e0564816c3c824a06b697438048deca8
kanogithub <linkano.teck@gmail.com>
2022-04-21 16:29:51 +1000

    hooks demo start v.3

---
+++ hooks-demo/src/components/UseRefExample1.jsx
@@ -0,0 +1,13 @@
+function UseRefExample1() {
+	return (
+		<div>
+			<form action=''>
+				<label htmlFor='name'>Name</label>
+				<input type='text' id='name' className='form-control-mb-2' />
+				<button className='btn btn-primary'></button>
+			</form>
+		</div>
+	)
+}
+
+export default UseRefExample1
