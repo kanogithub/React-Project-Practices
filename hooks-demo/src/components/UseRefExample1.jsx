14a3a329959138316ae62012075fccb2866a2016
kanogithub <linkano.teck@gmail.com>
2022-04-21 17:03:44 +1000

    useRef example 1

--- hooks-demo/src/components/UseRefExample1.jsx
+++ hooks-demo/src/components/UseRefExample1.jsx
@@ -1,11 +1,34 @@
+import { useRef } from 'react'
+
 function UseRefExample1() {
+	// useRef can
+	// get value from DOM element as
+	// or directly implement DOM element
+	const inputRef = useRef()
+	const onSubmit = (e) => {
+		e.preventDefault()
+		console.log('previous value:', inputRef.current.value)
+		console.log('reset input value to Hello')
+		inputRef.current.value = 'Hello'
+		console.log('set input background color to #dedede')
+		inputRef.current.style.backgroundColor = '#dedede'
+	}
+
 	return (
-		<div>
-			<form action=''>
-				<label htmlFor='name'>Name</label>
-				<input type='text' id='name' className='form-control-mb-2' />
-				<button className='btn btn-primary'>Submit</button>
+		<div className='m-2'>
+			<form onSubmit={onSubmit}>
+				<label htmlFor='name' className='label'>
+					Name
+				</label>
+				<input
+					type='text'
+					id='name'
+					className='form-control mb-2 input-sm input input-bordered'
+					ref={inputRef}
+				/>
+				<button className='btn btn-sm btn-primary'>Submit</button>
 			</form>
+			<p onClick={() => inputRef.current.focus()}>hit me to focus</p>
 		</div>
 	)
 }
