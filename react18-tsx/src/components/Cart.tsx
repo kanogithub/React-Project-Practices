cad7203a9fe1752703641921db0a857af22688d9
kanogithub <linkano.teck@gmail.com>
2023-03-19 16:42:06 +1100

    move api to services

--- react18-tsx/src/components/Cart.tsx
+++ react18-tsx/src/components/Cart.tsx
@@ -1,16 +1,20 @@
 interface Props {
-    cartItems: string[],
-    onClear: () => void
+	cartItems: string[]
+	onClear: () => void
 }
 
 const Cart = ({ cartItems, onClear }: Props) => {
-    return (<>
-        <div>Cart</div>
-        <ul>
-            {cartItems.map(cart => <li>cart</li>)}
-        </ul>
-        <button onClick={onClear}>Clear</button>
-    </>)
+	return (
+		<>
+			<div>Cart</div>
+			<ul>
+				{cartItems.map((cart) => (
+					<li key={cart}>cart</li>
+				))}
+			</ul>
+			<button onClick={onClear}>Clear</button>
+		</>
+	)
 }
 
 export default Cart
--- react18-tsx/src/components/ListGroup/ListGroup.tsx
+++ react18-tsx/src/components/ListGroup/ListGroup.tsx
@@ -1,49 +1,48 @@
 import { useState } from 'react'
 import styles from './ListGroup.module.css'
-import styled from "styled-components";
+import styled from 'styled-components'
+import { User } from '../../App'
 
 interface ListItemProps {
-  active: boolean,
+	active: boolean
 }
 
 const List = styled.li<ListItemProps>`
-  padding: 0.25rem 0.75rem;
-  margin: 0.25rem;
-  border: solid 1px;
-  user-select: none;
-  background-color: ${props => props.active ? 'azure' : 'initial'}
+	padding: 0.25rem 0.75rem;
+	margin: 0.25rem;
+	border: solid 1px;
+	user-select: none;
+	background-color: ${(props) => (props.active ? 'azure' : 'initial')};
 `
 
 interface Props {
-  items: string[],
-  heading: string,
-  onSelectItem: (item: string) => void
+	items: User[]
+	heading: string
+	onSelectItem: (item: User) => void
 }
 
 function ListGroup({ items, heading, onSelectItem }: Props) {
+	const [selectedId, setSelectedId] = useState(-1)
 
-  const [selectedIndex, setSelectedIndex] = useState(-1)
-
-  return (
-    <div className='m-2'>
-      <h1>{heading}</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className={[styles.listGroup, styles.listBackground].join(' ')}>
-        {items.map((item, index) => (
-          <List
-            active={selectedIndex === index}
-            key={index}
-            onClick={() => {
-              setSelectedIndex(index)
-              onSelectItem(item)
-            }}>
-            {item}
-          </List>
-        ))}
-      </ul>
-    </div>
-  )
-
+	return (
+		<div className='m-2'>
+			<h1>{heading}</h1>
+			{items.length === 0 && <p>No item found</p>}
+			<ul className={[styles.listGroup, styles.listBackground].join(' ')}>
+				{items.map((item, index) => (
+					<List
+						active={selectedId === item.id}
+						key={index}
+						onClick={() => {
+							setSelectedId(item.id)
+							onSelectItem(item)
+						}}>
+						{item.name}
+					</List>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ListGroup
